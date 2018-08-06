6c13fd323170c43d30b2007ccacb86298c74de3d
Guerric Inselberger <guerric.insel@gmail.com>
2018-08-06 17:37:00 -0300

    Adicionar link da sinnapse no rodape

--- source/php/landing-page.php
+++ source/php/landing-page.php
@@ -84,31 +84,31 @@
 							<li>Covers AR, AP & Reporting for Brazil, Mexico, Chile, Argentina, Colombia, Ecuador, Uruguay & Peru</li>
 							<li>Use your investment in SAP</li>
 						</ul>
-					</div>
+					</div> <!-- /bloco -->
 					<div class="bloco">
 						<h3>Regional latin america platform</h3>
 						<ul>
 							<li>Covers AR, AP & Reporting for Brazil, Mexico, Chile, Argentina, Colombia, Ecuador, Uruguay & Peru</li>
 							<li>Use your investment in SAP</li>
 						</ul>
-					</div>
+					</div> <!-- /bloco -->
 					<div class="bloco">
 						<h3>Lower IT support & change management costs</h3>
 						<ul>
 							<li>Covers AR, AP & Reporting for Brazil, Mexico, Chile, Argentina, Colombia, Ecuador, Uruguay & Peru</li>
 							<li>Use your investment in SAP</li>
 						</ul>
-					</div>
+					</div> <!-- /bloco -->
 					<div class="bloco">
 						<h3>Lower IT support & change management costs</h3>
 						<ul>
 							<li>Covers AR, AP & Reporting for Brazil, Mexico, Chile, Argentina, Colombia, Ecuador, Uruguay & Peru</li>
 							<li>Use your investment in SAP</li>
 						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
+					</div> <!-- /bloco -->
+				</div> <!-- /row -->
+			</div> <!-- /container -->
+		</div> <!-- /beneficios -->
 	</section>
 	<section class="bg-creme">
 		<div class="clientes">
@@ -117,25 +117,25 @@
 				<div class="logos owl-carousel owl-theme">
 					<div class="item">
 						<img src="./dist/img/kelloggs.png" alt="Kellogg's logo">
-					</div>
+					</div> <!-- /item -->
 					<div class="item">
 						<img src="./dist/img/peg.png" alt="P&G logo">
-					</div>
+					</div> <!-- /item -->
 					<div class="item">
 						<img src="./dist/img/coca-cola.png" alt="Coca-Cola logo">
-					</div>
+					</div> <!-- /item -->
 					<div class="item">
 						<img src="./dist/img/lenovo.png" alt="Lenovo logo">
-					</div>
+					</div> <!-- /item -->
 					<div class="item">
 						<img src="./dist/img/siemens.png" alt="Siemens logo">
-					</div>
+					</div> <!-- /item -->
 					<div class="item">
 						<img src="./dist/img/philips.png" alt="Philips logo">
-					</div>
-				</div>
-			</div>
-		</div>
+					</div> <!-- /item -->
+				</div> <!-- /logos -->
+			</div> <!-- /container -->
+		</div> <!-- /clientes -->
 	</section>
 	<section>
 		<div class="webinar">
@@ -147,13 +147,13 @@
 						<a class="botão" href="#" target="_blank">
 							assita agora
 						</a>
-					</div>
+					</div> <!-- /texto -->
 					<div class="coluna imagem">
 						<img src="./dist/img/laptop.png" alt="Laptop com uma demonstração de como funciona o EFD-REINF">
-					</div>
-				</div>
-			</div>
-		</div>
+					</div> <!-- /imagem -->
+				</div> <!-- /row -->
+			</div> <!-- /container -->
+		</div> <!-- /webinar -->
 	</section>
 	<section class="bg-creme">
 		<div class="ebook">
@@ -161,17 +161,17 @@
 				<div class="row no-gutters">
 					<div class="coluna imagem">
 						<img src="./dist/img/ebook.png" alt="E-Book Guia Prático EFD - REINF">
-					</div>
+					</div> <!-- /imagem -->
 					<div class="coluna texto">
 						<h2>EFD-REINF</h2>
 						<span>Esclareça suas duvidas gratuitamente</span>
 						<a class="botão" href="#" target="_blank">
 							faça o download
 						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+					</div> <!-- /texto -->
+				</div> <!-- /row -->
+			</div> <!-- /container -->
+		</div> <!-- /ebook -->
 	</section>
 	<footer>
 		<div class="container">
@@ -179,13 +179,15 @@
 				<div class="esquerda">
 					<img src="./dist/img/logo-white.png" alt="Invoiceware by Sovos">
 					<span>Única Solução Global configurada para cada País.</span>
-				</div>
+				</div> <!-- /esquerda -->
 				<div class="direita">
-					<span>Desenvolvido por</span>
-					<img src="./dist/img/sinnapse-logo.png" alt="Sinnapse">
-				</div>
-			</div>
-		</div>
+					<a href="http://sinnapse.com" target="_blank">
+						<span>Desenvolvido por</span>
+						<img src="./dist/img/sinnapse-logo.png" alt="Sinnapse">
+					</a>
+				</div> <!-- /direita -->
+			</div> <!-- /row -->
+		</div> <!-- /container -->
 	</footer>
 	<script type="text/javascript" src="./dist/js/lib.js"></script>
 	<script type="text/javascript" src="./dist/js/app.min.js"></script>
